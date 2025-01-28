5f85b697c4cd16cdf596f0517a3f5c082c5b217d
evascoiann <kiEvasco@mcm.edu.ph>
2025-01-28 12:31:40 +0800

    first commit

---
+++ NameDateTime.java
@@ -0,0 +1,17 @@
+
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class NameDateTime {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Kristine Ianna A. Evasco");
+        
+        LocalDateTime myDateObj = LocalDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        
+        String formattedDate = myDateObj.format(myFormatObj);   
+        System.out.println("The current date and time is: " + formattedDate );
+    }
+}
